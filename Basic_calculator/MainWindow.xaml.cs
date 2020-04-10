f03c81a49fa7738fa357a34849bc2c864e6ec5a4
Bakos Bence <b.bakos98@gmail.com>
2020-04-10 16:16:58 +0200

    Created version alpha

---
+++ Basic_calculator/MainWindow.xaml.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Basic_calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double lastNumber, result;
+        SelectedOperator selectedOperator;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            acButton.Click += AcButton_Click;
+            negativeButton.Click += NegativeButton_Click;
+            equalButton.Click += EqualButton_Click;
+            dotButton.Click += DotButton_Click;
+        }
+        private void DotButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!resultLabel.Content.ToString().Contains("."))
+            {
+                resultLabel.Content = $"{resultLabel.Content}.";
+            }
+        }
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
+        {
+            double newNumber;
+
+            if (double.TryParse(resultLabel.Content.ToString(), out newNumber))
+            {
+                switch (selectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        result = SimpleMath.Add(lastNumber, newNumber);
+                        lastLabel.Content = $"{lastNumber}+{newNumber}";
+                        break;
+                    case SelectedOperator.Subtraction:
+                        result = SimpleMath.Subtract(lastNumber, newNumber);
+                        lastLabel.Content = $"{lastNumber}-{newNumber}";
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result = SimpleMath.Multiply(lastNumber, newNumber);
+                        lastLabel.Content = $"{lastNumber}*{newNumber}";
+                        break;
+                    case SelectedOperator.Divison:
+                        result = SimpleMath.Divide(lastNumber, newNumber);
+                        lastLabel.Content = $"{lastNumber}/{newNumber}";
+                        break;
+                    case SelectedOperator.Modulo:
+                        result = SimpleMath.Modulo(lastNumber, newNumber);
+                        lastLabel.Content = $"{lastNumber}%{newNumber}";
+                        break;
+                    default:
+                        break;
+                }
+                resultLabel.Content = result.ToString();
+            }
+        }
+        private void NegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber *= -1;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                resultLabel.Content = "0";
+                lastLabel.Content = lastNumber.ToString();
+            }
+
+            if (sender == multipleButton)
+                selectedOperator = SelectedOperator.Multiplication;
+            if (sender == divisionButton)
+                selectedOperator = SelectedOperator.Divison;
+            if (sender == plusButton)
+                selectedOperator = SelectedOperator.Addition;
+            if (sender == minusButton)
+                selectedOperator = SelectedOperator.Subtraction;
+            if (sender == moduloButton)
+                selectedOperator = SelectedOperator.Modulo;
+        }
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            byte selectedValue = 0;
+
+            if (sender == nullButton)
+                selectedValue = 0;
+            if (sender == oneButton)
+                selectedValue = 1;
+            if (sender == twoButton)
+                selectedValue = 2;
+            if (sender == threeButton)
+                selectedValue = 3;
+            if (sender == fourButton)
+                selectedValue = 4;
+            if (sender == fiveButton)
+                selectedValue = 5;
+            if (sender == sixButton)
+                selectedValue = 6;
+            if (sender == sevenButton)
+                selectedValue = 7;
+            if (sender == eightButton)
+                selectedValue = 8;
+            if (sender == nineButton)
+                selectedValue = 9;
+
+
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = $"{selectedValue}";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
+            }
+        }
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
+            lastLabel.Content = "";
+        }
+    }
+
+    public enum SelectedOperator { Addition, Subtraction, Multiplication, Divison, Modulo}
+
+    public class SimpleMath
+    {
+        public static double Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        public static double Subtract(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+        public static double Divide(double n1, double n2)
+        {
+            if (n2 == 0)
+            {
+                MessageBox.Show("Division by 0 is not supported", "Wrong operation", MessageBoxButton.OK, MessageBoxImage.Error);
+                return 0;
+            }
+            return n1 / n2;
+        }
+        public static double Multiply(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+        public static double Modulo(double n1, double n2)
+        {
+            if (n2 == 0)
+            {
+                MessageBox.Show("Modulo with 0 is not supported", "Wrong operation", MessageBoxButton.OK, MessageBoxImage.Error);
+                return 0;
+            }
+            return n1 % n2;
+        }
+    }
+}
